4741c8933cf353554d6eba73aa8fa80abe40c09a
bigbigbo <zxb141242@163.com>
2022-03-11 13:53:11 +0800

    feat: plaintext 组件新增 placeholder 和 hidePlaceholder 属性

--- src/PlainText.tsx
+++ src/PlainText.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 
 export interface IProps extends React.HTMLAttributes<HTMLSpanElement> {
   value?: string;
+  hidePlaceholder?: boolean;
 }
 
 const PlainText = React.forwardRef<any, IProps>((props, ref) => {
-  const { value, ...spanProps } = props;
+  const { value, hidePlaceholder = false, placeholder = '-', ...spanProps } = props;
 
   return (
     <span ref={ref} {...spanProps}>
-      {value}
+      {value || (!hidePlaceholder && placeholder)}
     </span>
   );
 });
