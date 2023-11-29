0c563479a3789798f2f5342eef8531c7a845744d
bigbigbo <zxb141242@163.com>
2023-11-29 14:01:37 +0800

    feat: 多语言适配nextjs

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,7 +4,12 @@ import zhCN from './locale/zh-CN';
 import enUS from './locale/en-US';
 
 // NOTE: 约定从 localStorage 中去读取 siteLang
-const siteLang = localStorage.getItem('siteLang') || 'zh-CN';
+let siteLang = 'zh-CN';
+
+// NOTE: 适配 nextjs
+if (typeof window !== 'undefined') {
+  siteLang = localStorage.getItem('siteLang') || 'zh-CN';
+}
 
 const kiwiIntl = KiwiIntl.init(siteLang, {
   'en-US': enUS,
